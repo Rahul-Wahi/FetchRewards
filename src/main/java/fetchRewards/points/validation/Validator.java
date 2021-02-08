4f3c970c88fcef4e42f778a8e7cfe452d0e4a4d3
Rahul Wahi <rahulwahi42@gmail.com>
2021-02-08 17:21:22 -0500

    adding validator for validating the request body

---
+++ src/main/java/fetchRewards/points/validation/Validator.java
@@ -0,0 +1,43 @@
+package fetchRewards.points.validation;
+
+import fetchRewards.points.entity.DeductPoint;
+import fetchRewards.points.entity.Point;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Validator {
+
+    public void validateAddPoint(Point point) {
+        String payer = point.getPayer();
+        Integer points = point.getPoints();
+
+        if(payer == null || payer.trim().isEmpty()) {
+            throw new IllegalArgumentException("Please provide payer name");
+        }
+
+        if (points == null) {
+            throw new IllegalArgumentException("Please provide points to add");
+        }
+
+        if (points <= 0) {
+            throw new IllegalArgumentException("Number of points should pe positive");
+        }
+    }
+
+    public void validateDeductPoint(DeductPoint deductPoint, int totalBalance) {
+        Integer points = deductPoint.getPoints();
+
+        if (points == null) {
+            throw new IllegalArgumentException("Please provide points to deduct");
+        }
+
+        if (points <= 0) {
+            throw new IllegalArgumentException("Number of points to be deducted should pe positive");
+        }
+
+        if (points > totalBalance) {
+            throw new IllegalArgumentException("Number of points to be deducted should be less than the total balance : "
+                    + totalBalance);
+        }
+    }
+}
