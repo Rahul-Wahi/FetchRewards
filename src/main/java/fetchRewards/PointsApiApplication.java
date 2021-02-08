99a76294c21ba9d95aebc2b2f51a45a4ddd3c6cd
Rahul Wahi <rahulwahi42@gmail.com>
2021-02-08 04:44:41 -0500

    SpringBoot Main class

---
+++ src/main/java/fetchRewards/PointsApiApplication.java
@@ -0,0 +1,12 @@
+package fetchRewards;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PointsApiApplication.class, args);
+    }
+
+}
