c495056edb359591eae4ad17a8b954961eb439aa
Rahul Wahi <rahulwahi42@gmail.com>
2021-02-08 04:46:41 -0500

    Class for deduct point

---
+++ src/main/java/fetchRewards/points/DeductPoint.java
@@ -0,0 +1,14 @@
+package fetchRewards.points;
+
+public class DeductPoint {
+    private Integer points;
+
+    public Integer getPoints() {
+        return points;
+    }
+
+    public void setPoints(Integer points) {
+        this.points = points;
+    }
+
+}
