b0093a39b1ccb8913de8df3efe6208d31a48e773
Rahul Wahi <rahulwahi42@gmail.com>
2021-02-08 17:20:54 -0500

    adding class for error message

---
+++ src/main/java/fetchRewards/points/response/ErrorMessage.java
@@ -0,0 +1,29 @@
+package fetchRewards.points.response;
+
+
+public class ErrorMessage {
+    Integer code;
+    String message;
+
+    public ErrorMessage(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
