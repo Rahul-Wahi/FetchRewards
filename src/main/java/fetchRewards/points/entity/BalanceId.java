1ca87f006597fdffbc5a6a1dd2574a8fbf20df0d
Rahul Wahi <rahulwahi42@gmail.com>
2021-02-08 17:22:42 -0500

    adding class for creating the composite primary key for Point Balance

---
+++ src/main/java/fetchRewards/points/entity/BalanceId.java
@@ -0,0 +1,32 @@
+package fetchRewards.points.entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class BalanceId implements Serializable {
+
+    String payer;
+    String customer;
+
+    public BalanceId() {
+    }
+
+    public BalanceId(String payer, String customer) {
+        this.payer = payer;
+        this.customer = customer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BalanceId balanceId = (BalanceId) o;
+        return Objects.equals(payer, balanceId.payer) &&
+                Objects.equals(customer, balanceId.customer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(payer, customer);
+    }
+}
